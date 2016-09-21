a7a3e19c5d9113944f75f7ce45996d0993434fd9
Brian Anderson <bqanderson@me.com>
2016-09-20 19:43:20 -0500

    add nodemailer-smtp-transport

--- routes/sendEmail.js
+++ routes/sendEmail.js
@@ -1,5 +1,6 @@
 var router = require('express').Router();
 var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
 
 var Guest = require('../models/guest.js');
 
@@ -8,18 +9,18 @@ router.post('/', function(req, res){
 
   console.log('Data sent:', data);
 
-  var transporter = nodemailer.createTransport({
-    service: 'Horde',
+  var transporter = nodemailer.createTransport(smtpTransport({
+    service: 'gmail',
     auth: {
       user: process.env.username,
       pass: process.env.password
     }
-  });
+  }));
   var mailOptions = {
-    from: 'Bella Luna Studios at The Wolf House NE',
+    from: '"Bella Luna Studios at The Wolf House NE" <info.bellalunastudios@gmail.com>',
     to: data.emailList,
     subject: data.subject,
-    text: data.message + '\n' + data.link
+    text: data.message + '\n\n' + data.link
   };
 
   transporter.sendMail(mailOptions, function(error, info){

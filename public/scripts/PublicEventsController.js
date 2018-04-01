8dfc999a82deeb07b25c9b5a31d25640593a56ac
bqanderson <bqanderson@me.com>
2018-04-01 11:06:49 -0500

    Update calander to include Airbnb link. Lineter fixes.

--- public/scripts/PublicEventsController.js
+++ public/scripts/PublicEventsController.js
@@ -1,34 +1,41 @@
-angular.module('publicApp').controller('PublicEventsController', function($http, moment, alert, calendarConfig, PubDataService){
-  var vm = this;
-  vm.allEvents = PubDataService.allEvents;
-  vm.upcomingEvents = PubDataService.newEvents;
-  vm.pastEvents = PubDataService.pastEvents;
+angular.module('publicApp')
+  .controller('PublicEventsController',
+  function($http, $window, moment, alert, calendarConfig, PubDataService){
+    var vm = this;
+    vm.allEvents = PubDataService.allEvents;
+    vm.upcomingEvents = PubDataService.newEvents;
+    vm.pastEvents = PubDataService.pastEvents;
 
-  //Angular-Calendar
-  vm.calendarView = 'month';
-  vm.viewDate = new Date();
+    //Angular-Calendar
+    vm.calendarView = 'month';
+    vm.viewDate = new Date();
 
-  // Toggle Calendar Event
-  vm.toggle = function($event, field, event) {
-    $event.preventDefault();
-    $event.stopPropagation();
-    event[field] = !event[field];
-  };
+    // Toggle Calendar Event
+    vm.toggle = function($event, field, event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+      event[field] = !event[field];
+    };
 
-  // Open View Event Modal
-  vm.viewEventClicked = function(sendData) {
-    alert.showEvent(sendData);
-  };
+    // Open View Event Modal
+    vm.viewEventClicked = function(sendData) {
+      alert.showEvent(sendData);
+    };
 
-  //Open Suggest Event Modal
-  vm.suggestEvent = function(){
-    alert.suggestEvent();
-  }
+    //Open Suggest Event Modal
+    vm.suggestEvent = function(){
+      alert.suggestEvent();
+    }
 
-  //Open Request Modal
-  vm.requestAIR = function(){
-    alert.requestAIR();
-  }
+    //Open Request Modal
+    vm.requestAIR = function(){
+      alert.requestAIR();
+    }
 
-  PubDataService.showEvents();
-})
+    // Redirect to Airbnb
+    vm.redirectToAirbnb = function(){
+      $window.open('https://www.airbnb.com/rooms/10544685?eluid=0&euid=c970897f-59ee-4f22-b90f-38c83bae4ce1&s=22&user_id=3527145');
+    }
+
+    PubDataService.showEvents();
+  })
